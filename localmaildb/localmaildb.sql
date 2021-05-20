1d988aa9863fb311d21fe1125bd23841a0e72d15
George Dunlap <george.dunlap@citrix.com>
2021-05-20 16:07:05 +0100

    localmaildb: Add database query recipes
    Signed-off-by: George Dunlap <george.dunlap@citrix.com>

--- localmaildb/localmaildb.sql
+++ localmaildb/localmaildb.sql
@@ -47,3 +47,79 @@ create table if not exists lmdb_mailbox_join(
     messageid text,
     foreign key(mailboxid) references lmdb_mailboxes,
     foreign key(messageid) references lmdb_messages);
+
+/*
+ * RECIPES
+ */
+
+/* Date, sender, subject */
+select date,
+       personalname,
+       mailboxname,
+       hostname,
+       subject
+    from lmdb_messages
+      natural join lmdb_envelopejoin
+      natural join lmdb_addresses
+    where envelopepart=1;
+
+/* Count sender emails */
+select mailboxname, hostname, count(*) as n
+    from lmdb_messages
+      natural join lmdb_envelopejoin
+      natural join lmdb_addresses
+    where envelopepart=1
+    group by mailboxname,hostname
+    order by n desc
+    limit 30;
+
+/* Histogram of messages sent by month */
+select strftime("%Y-%m", date) as ts,
+       count(*)
+  from lmdb_messages
+  group by ts
+  order by ts desc;
+
+/* Histogram of messages per month, sent by addresses tagged as 'bot' */
+select strftime("%Y-%m", date) as ts,
+       count(*)
+  from lmdb_messages
+    natural join lmdb_envelopejoin
+    natural join lmdb_addresses
+    natural join idmap.address_to_tag
+    natural join idmap.tags
+  where envelopepart=1 /* Restrict to 'From' addresses */
+    and tagname='bot'
+  group by ts
+  order by ts desc;
+
+/* All addresses not tagged as 'bot' */
+select mailboxname, hostname
+   from lmdb_addresses
+     left natural join
+       (select *
+          from idmap.address_to_tag
+	    natural join idmap.tags
+	    where tagname='bot')
+    where tagid is NULL
+    limit 20;
+       
+
+/* Histogram of messages per month, sent by addresses tagged as 'bot' */
+with nonbot_addresses as
+  (select * from lmdb_addresses
+     left natural join
+       (select *
+          from idmap.address_to_tag
+	    natural join idmap.tags
+	    where tagname='bot')
+    where tagid is NULL)
+select strftime("%Y-%m", date) as ts,
+       count(*)
+  from lmdb_messages
+    natural join lmdb_envelopejoin
+    natural join nonbot_addresses
+  where envelopepart=1 /* Restrict to 'From' addresses */
+  group by ts
+  order by ts desc;
+
