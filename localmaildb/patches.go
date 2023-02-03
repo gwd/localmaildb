401af302e4dc464227b6235a2525906393ad9f6a
George Dunlap <george.dunlap@cloud.com>
2023-02-03 13:54:31 +0000

    localmaildb: Handle more "tree" types
    Add patch-parsing of subject.
    
    Use this in TreeAm to pick out the appropriate patches.
    
    Signed-off-by: George Dunlap <george.dunlap@cloud.com>

--- localmaildb/patches.go
+++ localmaildb/patches.go
@@ -1,17 +1,62 @@
 package localmaildb
 
+import "regexp"
+
 // Things to handle:
 // - Only a single email, top-level has [PATCH] (or [PATCH v2])
 // - Thread with [0/N] at the top
 // - Thread with [1/N] at the top
+type PatchMailType int
+
+const (
+	PatchMailNone      = PatchMailType(0)    // Mail does not seem to be a patch
+	PatchMailSingleton = PatchMailType(iota) // Mail seems to be a singleton patch
+	PatchMail0N        = PatchMailType(iota) // Mail seems to be a cover letter (0/N)
+	PatchMailMN        = PatchMailType(iota) // Mail seems to be a patch in a series (M/N, with 0<M<=N)
+)
+
+var rePatchMail = map[PatchMailType]*regexp.Regexp{
+	PatchMailSingleton: regexp.MustCompile(`^\[(RFC )?PATCH( RFC| v[0-9]+)*\]`),
+	PatchMail0N:        regexp.MustCompile(`^\[(RFC )?PATCH( RFC| v[0-9]+)* 0+/[0-9]+\]`),
+	PatchMailMN:        regexp.MustCompile(`^\[(RFC )?PATCH( RFC| v[0-9]+)* 0*[0-9]+0*/[0-9]+\]`),
+}
+
+func SubjectDetectPatch(s string) PatchMailType {
+	for k, re := range rePatchMail {
+		if re.MatchString(s) {
+			return k
+		}
+	}
+	return PatchMailNone
+}
+
+func appendMessage(mtp *[]*MessageTree, msg *MessageTree) {
+	message := *msg
+	message.Replies = nil
+
+	*mtp = append(*mtp, &message)
+}
+
 func TreeFilterAm(tree *MessageTree) []*MessageTree {
 	var mt []*MessageTree
 
-	for _, reply := range tree.Replies {
-		message := *reply
-		message.Replies = nil
-
-		mt = append(mt, &message)
+	switch SubjectDetectPatch(tree.Envelope.Subject) {
+	case PatchMailNone:
+		return nil
+	case PatchMailSingleton:
+		appendMessage(&mt, tree)
+	case PatchMailMN:
+		// If the root is M/N, add the root and fall through
+		appendMessage(&mt, tree)
+		fallthrough
+	case PatchMail0N:
+		// If the root is 0/N, skip the root, but include all replies which are M/N.
+		// If the root is M/N, include all replies which are M/N.
+		for _, reply := range tree.Replies {
+			if SubjectDetectPatch(reply.Envelope.Subject) == PatchMailMN {
+				appendMessage(&mt, reply)
+			}
+		}
 	}
 
 	return mt
---
+++ localmaildb/patches_test.go
@@ -0,0 +1,29 @@
+package localmaildb
+
+import "testing"
+
+func TestSubjectClassifier(t *testing.T) {
+	tests := []struct {
+		subject string
+		want    PatchMailType
+	}{
+		{"[PATCH] xen/common: Constify the parameter of _spin_is_locked()", PatchMailSingleton},
+		{"[PATCH v2] Add more rules to docs/misra/rules.rst", PatchMailSingleton},
+		{"[PATCH RFC] Add more rules to docs/misra/rules.rst", PatchMailSingleton},
+		{"[RFC PATCH] Add more rules to docs/misra/rules.rst", PatchMailSingleton},
+		{"[PATCH RFC v2] Add more rules to docs/misra/rules.rst", PatchMailSingleton},
+		{"[PATCH v4 07/11] xen: add cache coloring allocator for domains", PatchMailMN},
+		{"[RFC PATCH 01/10] xen: pci: add per-domain pci list lock", PatchMailMN},
+		{"[RFC PATCH 10/10] xen: pci: add per-domain pci list lock", PatchMailMN},
+		{"[RFC PATCH 0/8] SVE feature for arm guests", PatchMail0N},
+		{"[PATCH v2 00/41] xen/arm: Add Armv8-R64 MPU support to Xen - Part#1", PatchMail0N},
+	}
+
+	for _, test := range tests {
+		got := SubjectDetectPatch(test.subject)
+		if got != test.want {
+			t.Errorf("ERROR: Subject %s: want %v got %v!", test.subject, test.want, got)
+		}
+	}
+
+}
